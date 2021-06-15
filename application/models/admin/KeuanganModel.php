20217202686b261222694aec45460c57d2c26de1
naurahgrdnfa <54610803+naurahgrdnfa@users.noreply.github.com>
2021-06-15 10:04:27 +0700

    Update KeuanganModel.php

--- application/models/admin/KeuanganModel.php
+++ application/models/admin/KeuanganModel.php
@@ -8,7 +8,7 @@ class KeuanganModel extends CI_Model
 	public function getKeuangan()
 	{
 		$this->db->select('bulan, tahun,jumlah_warga, jumlah_sudah_bayar, 
- 						jumlah_belum_bayar,saldo,cek_pengeluaran(bulan,tahun) as pengeluaran,cek_pemasukan(bulan,tahun) as pemasukan,total_saldo');
+ 						jumlah_belum_bayar,cek_iuran(bulan,tahun) as saldo,cek_pengeluaran(bulan,tahun) as pengeluaran,cek_pemasukan(bulan,tahun) as pemasukan,total_saldo,cek_tagihan(bulan,tahun) as tagihan');
 		$this->db->from('data_keuangan_iuran');
 		$this->db->where_not_in('tahun', '0000');
 		return $this->db->get();
@@ -28,9 +28,30 @@ class KeuanganModel extends CI_Model
 		$this->db->where('tahun_iuran', $tahun);
 		return $this->db->get('data_iuran_warga');
 	}
+	
+	public function updateSaldoPem($bulan,$tahun,$data)
+	{
+		$this->db->where('bulan', $bulan);
+		$this->db->where('tahun', $tahun);
+		$this->db->update('data_keuangan_iuran', $data);
+	}
+	public function updateSaldoPeng($bulan,$tahun,$data)
+	{
+		$this->db->where('bulan', $bulan);
+		$this->db->where('tahun', $tahun);
+		$this->db->update('data_keuangan_iuran', $data);
+	}
 
 	public function updateSaldo($data)
 	{
 		$this->db->update('data_keuangan_iuran', $data);
+	}
+	public function check_bulan_tahun($bulan,$tahun)
+	{
+		$this->db->select('bulan , tahun');
+		$this->db->from('data_keuangan_iuran');
+		$this->db->where('bulan', $bulan);
+		$this->db->where('tahun', $tahun);
+		return $this->db->get();
 	}
 }
