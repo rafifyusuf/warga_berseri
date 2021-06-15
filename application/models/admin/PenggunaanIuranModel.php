3327cca227760b25c869bb06036d1fe18f33209c
naurahgrdnfa <54610803+naurahgrdnfa@users.noreply.github.com>
2021-06-15 10:07:20 +0700

    Update PenggunaanIuranModel.php

--- application/models/admin/PenggunaanIuranModel.php
+++ application/models/admin/PenggunaanIuranModel.php
@@ -8,8 +8,9 @@ class PenggunaanIuranModel extends CI_Model
 		$this->db->insert('data_penggunaan_iuran', $data);
 	}
 
-	public function tampilDataPenggunaan()
+	public function tampilDataPenggunaan($bulan)
 	{
+		$this->db->where('bulan_penggunaan',$bulan);
 		return $this->db->get('data_penggunaan_iuran');
 	}
 
@@ -29,5 +30,13 @@ class PenggunaanIuranModel extends CI_Model
 	{
 		$this->db->where('id_penggunaan', $id);
 		return $this->db->get('data_penggunaan_iuran');
+	}
+	
+	public function penggunaan_bulanan()
+	{
+		$this->db->select('bulan_penggunaan , SUM(jumlah_pengeluaran) as pengeluaran' );
+		$this->db->from('data_penggunaan_iuran');
+		$this->db->group_by('bulan_penggunaan');
+		return $this->db->get();
 	}
 }
