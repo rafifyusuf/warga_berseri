0b81e4ffda620b422f99afbf105b81c349b30c80
naurahgrdnfa <54610803+naurahgrdnfa@users.noreply.github.com>
2021-06-24 10:37:53 +0700

    Update Iuran.php

--- application/controllers/admin/Iuran.php
+++ application/controllers/admin/Iuran.php
@@ -55,8 +55,7 @@ class Iuran extends CI_Controller
 		$this->upload->initialize($config);
 
 		if (!$this->upload->do_upload('bukti_penggunaan')) {
-			$error = array('error' => $this->upload->display_errors());
-			print_r($error);
+			echo "<script>alert('Gagal di tambahkan! Periksa kembali file yang anda masukan');history.go(-1);</script>";
 		} else {
 			$data = array('upload_data'	=>	$this->upload->data());
 			$admin = $this->session->all_userdata();
